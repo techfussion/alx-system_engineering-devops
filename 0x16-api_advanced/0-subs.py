a4755139e765afabfd59a264099c27a1c237c99f
techfussion <muminraj15@gmail.com>
2024-08-28 16:22:23 -0700

    fix 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""Fetch subreddit subs"""
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Fetch reddit subs"""
-    url = f'https://api.reddit.com/r/{subreddit}/about'
-    headers = {'User-Agent': 'gritnec'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json() or {}
-        return data.get('data', {}).get('subscribers', 0)
-    return 0
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
